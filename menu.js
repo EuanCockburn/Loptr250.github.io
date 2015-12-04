6de0f88cdabccb130787e4016b1d6f1175a490f7
Euan Cockburn <2033584c@bo620-16u.(none)>
2015-12-04 11:57:34 +0000

    Test commit

---
+++ menu.js
@@ -0,0 +1,18 @@
+function setupChecker() {
+    sessionStorage.clear();
+    
+    
+    
+    for (var i = 1; i <= 4; i++) {
+        var radioName = "q" + i;
+        radioOptions = document.getElementsByName(radioName);
+        for (var j = 0; j < radioOptions.length; j++) {
+            if (radioOptions[j].checked) {
+                radioVal = radioOptions[j].value;
+                sessionStorage.setItem(radioName, radioVal);
+            }
+        }
+    }
+    
+    window.location.assign("results.html");
+}
